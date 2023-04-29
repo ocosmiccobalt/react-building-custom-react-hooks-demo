1554dcf9d1e920c1afbbeaad996e3c5f9b3c6cff
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-04-29 18:16:58 +0300

    Set up project for a more realistic example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
-import ForwardCounter from './components/ForwardCounter';
-import BackwardCounter from './components/BackwardCounter';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
 
-const App = () => (
-  <Fragment>
-    <ForwardCounter />
-    <BackwardCounter />
-  </Fragment>
-);
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        'https://react-http-23-default-rtdb.firebaseio.com/tasks.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </Fragment>
+  );
+};
 
 export default App;
