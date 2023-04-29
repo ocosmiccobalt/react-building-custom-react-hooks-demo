09eb324c2c535f5b60cdfe9f40465a439c811089
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-04-29 13:12:07 +0300

    Add components: ForwardCounter, BackwardCounter, Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-function App() {
-  return (
-    <div>Hello!</div>
-  );
-}
+import { Fragment } from 'react';
+
+import ForwardCounter from './components/ForwardCounter';
+import BackwardCounter from './components/BackwardCounter';
+
+const App = () => (
+  <Fragment>
+    <ForwardCounter />
+    <BackwardCounter />
+  </Fragment>
+);
 
 export default App;
